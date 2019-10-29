01c9bedee3aa5bc2d69199457edf8dd22f8a2507
phatcharaphon <35365031+terkzayo@users.noreply.github.com>
2019-10-29 14:42:29 +0700

    Using handlebars

--- main_script/create-default-password.js
+++ main_script/create-default-password.js
@@ -1,20 +1,15 @@
-const sqlite3 = require('sqlite3');
-let db = new sqlite3.Database('classicmodels.sqlite');
-
 var cryptoJS = require("crypto-js");
 
-console.log(cryptoJS.HmacMD5("123456","cpe342").toString());
+const encrypted = cryptoJS.AES.encrypt("1234", "cpe342");
+const decrypted = cryptoJS.AES.decrypt(encrypted, "cpe342");
 
-// const encrypted = cryptoJS.AES.encrypt("123456", "cpe342");
-// // const decrypted = cryptoJS.AES.decrypt(encrypted, "cpe342");
+console.log(encrypted.toString());
+console.log(decrypted.toString(cryptoJS.enc.Utf8));
 
-// // console.log(encrypted);
-// const a = encrypted.toString();
-// console.log(a);
-// console.log(decrypted.toString(cryptoJS.enc.Utf8));
 
-// db.run("update employees set password = ?", a, (err) => {
-//     if(err)console.log(err.message);
-//     console.log(`Rows inserted ${this.changes}`)
-// }
-// );
+// $.ajax({
+//     url: 'employees/login/createPassword',
+//     type: 'POST',
+//     dataType: 'json',
+//     success: ''
+// });
--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/database');
 const Products = require('../models/products');
 
 router.get('/', (req, res) => Products.findAll()
